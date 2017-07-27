f69c7f32fbbb05458a99b9f695518a9a14fc9983
Finn Woelm <public@finnwoelm.com>
2017-07-27 20:57:04 +0700

    Fix: Add 1 sec delay to our test suite for Sauce Labs
    Sauce Labs tests appear to have random failures due to our tests being
    executed too quickly. Assumably, the browser window is closed before
    Sauce Labs registers the result. Adding a 1 second delay to our test
    suite fixes the problem.
    
    This commit should be undone once we have enough tests to no longer need
    an extra delay.

---
+++ tests/acceptance/saucelabs-fix.js
@@ -0,0 +1,30 @@
+import { describe, it, beforeEach, afterEach, done } from 'mocha';
+import startApp from 'upshift/tests/helpers/start-app';
+import destroyApp from 'upshift/tests/helpers/destroy-app';
+
+describe('Sauce Labs Fix', function() {
+  let application;
+
+  beforeEach(function() {
+    application = startApp();
+  });
+
+  afterEach(function() {
+    destroyApp(application);
+  });
+
+  /**
+   * This is the most pointless acceptance test. We are just making everything
+   * wait for a second. This is because our few existing tests sometimes execute
+   * so fast that Sauce Labs considers them as failed when they have actually
+   * passed.
+   *
+   * We can probably remove this once we have some more tests.
+   **/
+
+  it('should wait a second to make sure Sauce Labs registers the result', function() {
+    setTimeout(function(){
+      done();
+    }, 1000);
+  });
+});
