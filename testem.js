91f4f2c7eebd08e519dbf8630768b071124fe410
Finn Woelm <public@finnwoelm.com>
2017-07-28 08:14:15 +0700

    Revert "Merge branch 'setup-saucelabs' into development"
    Sauce Labs tests randomly fail. The tests complete successfully in the
    browser but Sauce Labs sees them as having failed. The suspicion is that
    it is related to Mocha and its reporting not working with Sauce Labs
    with our current configuration. Something to be revisited at a later
    point...
    
    This reverts commit 504c2abc7ecac520d37b84a3cd8195377df26aef, reversing
    changes made to e918fc7107d19100622bf5cad364ec8cea811324.

--- testem.js
+++ testem.js
@@ -2,60 +2,11 @@
 module.exports = {
   test_page: 'tests/index.html?hidepassed',
   disable_watching: true,
-  parallel: 6,
-
   launch_in_ci: [
-    "SL_Safari_Current",
-    "SL_MS_Edge",
-    "SL_IE_11",
-    "SL_IE_10",
-    "SL_IE_9",
-    "PhantomJS"
-  ],
-
-  launch_in_dev: [
     'PhantomJS'
   ],
-
-  "launchers": {
-    SL_Safari_Current: {
-      "command": "ember sauce:launch -b safari -v latest --visibility public --no-connect --url <url>",
-      "protocol": "tap"
-    },
-
-    SL_MS_Edge: {
-      "command": "ember sauce:launch -b microsoftedge -v latest --visibility public --no-connect -u '<url>'",
-      "protocol": "tap"
-    },
-
-    SL_IE_11: {
-      "command": "ember sauce:launch -b 'internet explorer' -v 11 --visibility public --no-connect -u '<url>'",
-      "protocol": "tap"
-    },
-
-    SL_IE_10: {
-      "command": "ember sauce:launch -b 'internet explorer' -v 10 --visibility public --no-connect -u '<url>'",
-      "protocol": "tap"
-    },
-
-    SL_IE_9: {
-      "command": "ember sauce:launch -b 'internet explorer' -v 9 --visibility public --no-connect -u '<url>'",
-      "protocol": "tap"
-    },
-
-    SL_Chrome_Current: {
-      "command": "ember sauce:launch -- -p 'Windows 10' -b chrome -v latest --no-connect -u '<url>'",
-      "protocol": "tap"
-    },
-
-    SL_Firefox_Current: {
-      "command": "ember sauce:launch -- -p 'Windows 10' -b firefox -v latest --no-connect -u '<url>'",
-      "protocol": "tap"
-    },
-
-    SL_Safari_Last: {
-      "command": "ember sauce:launch -- -b safari -v 8 --no-connect -u '<url>'",
-      "protocol": "tap"
-    },
-  },
+  launch_in_dev: [
+    'PhantomJS',
+    'Chrome'
+  ]
 };
